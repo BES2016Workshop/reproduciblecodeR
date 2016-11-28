3dde2146aacbae607d131c1fb42fa5686e65962f
laurajanegraham <laurajaneegraham@gmail.com>
2016-11-28 15:38:47 +0000

    create messed up iris file

---
+++ assets/mess_up_iris.R
@@ -0,0 +1,7 @@
+library(tidyr)
+library(readr)
+iris_mess <- unite(iris, Sepal.Length_Sepal.Width, Sepal.Length, Sepal.Width) %>%
+  unite(Petal.Length_Petal.Width, Petal.Length, Petal.Width) %>%
+  gather(measurement, value, -Species)
+
+write.csv(iris_mess, file="iris.csv")
